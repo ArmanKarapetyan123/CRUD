4d078d3ac1d08bfcd61d8ac7861390b21fcaabbf
ArmanKarapetyan123 <armankarapetyan563@gmail.com>
2024-09-03 22:30:22 +0400

    initial commit

--- src/routes/productRouts.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { getProducts, getProductById, createProduct, updateProductById, deleteProductById } from "../controllers/productsController";
-export const ProductRouter = Router();
-ProductRouter.get("/", getProducts);
-ProductRouter.get("/:id", getProductById);
-ProductRouter.post("/", createProduct);
-ProductRouter.put("/:id", updateProductById);
-ProductRouter.delete("/:id", deleteProductById);
